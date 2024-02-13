e922d62533e9d141aebf93a107689834093e2569
Eduardo Villa <151227243+evilla092@users.noreply.github.com>
2024-02-13 19:05:10 +0000

    finished

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,50 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    @actor = Actor.new
+    @actor.name = params.fetch("query_name")
+    @actor.dob = params.fetch("query_dob")
+    @actor.bio = params.fetch("query_bio")
+    @actor.image = params.fetch("query_img_url")
+
+    if @actor.valid?
+      @actor.save
+      redirect_to("/actors", { :notice => "Actor created successfully." })
+    else
+      redirect_to("/actors", { :notice => "Actor failed to create successfully." })
+    end
+
+    
+  end
+
+
+  def update
+    the_id = params.fetch("path_id")
+    @actor = Actor.where({ :id => the_id }).at(0)
+
+    @actor.name = params.fetch("query_name")
+    @actor.dob = params.fetch("query_dob")
+    @actor.bio = params.fetch("query_bio")
+    @actor.image = params.fetch("query_image")
+
+    if @actor.valid?
+      @actor.save
+      redirect_to("/actors/#{@actor.id}", { :notice => "Actors updated successfully."} )
+    else
+      redirect_to("/actors/#{@actor.id}", { :alert => "Actors failed to update successfully." })
+    end
+  end
+
+
+  def destroy
+    the_id = params.fetch("path_id")
+    @actor = Actor.where({ :id => the_id }).at(0)
+
+    @actor.destroy
+
+    redirect_to("/actors", { :notice => "Actor deleted successfully."} )
+  end
+
 end
